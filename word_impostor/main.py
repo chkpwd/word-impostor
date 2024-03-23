09d4bdbd069cc8a1eec743db53353f2ef93f8098
Bryan Jones <unix.chkpwd@gmail.com>
2024-03-23 16:41:37 -0400

    add env for port and address

--- word_impostor/main.py
+++ word_impostor/main.py
@@ -29,11 +29,23 @@ if __name__ == "__main__":
     logger.info("Starting Game...")
 
     word_language = os.environ.get("WORD_LANGUAGE", "en")
+    listen_address = os.environ.get("ADDRESS", "0.0.0.0")
+    listen_port = os.environ.get("PORT", 5000)
 
     if os.environ.get("WORD_LANGUAGE") is None:
         logger.info("Language is not set. Defaulting to English.")
     else:
         logger.info("Language is set to '%s'", word_language)
+
+    if os.environ.get("ADDRESS") is None:
+        logger.info("Address is not set. Defaulting to 0.0.0.0.")
+    else:
+        logger.info("Address is set to '%s'", listen_address)
+
+    if os.environ.get("PORT") is None:
+        logger.info("Port is not set. Defaulting to 5000.")
+    else:
+        logger.info("Port is set to '%s'", listen_port)
 
     word_data = WordDataResponse(word_language)
     image_data = ImageDataResponse()
